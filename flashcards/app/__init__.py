1f0da7bee4578d192fe93240f3232d2a53a81376
smfasihaly <smfasihali@gmail.com>
2024-06-29 01:49:01 +0200

    fix multiple dataentry problem

--- flashcards/app/__init__.py
+++ flashcards/app/__init__.py
@@ -16,15 +16,17 @@ def create_app():
     app.register_blueprint(main_blueprint)
     
     with app.app_context():
-        if table_exists('words'):
+        if table_exists('words') and is_table_empty():
             import_data_from_excel(app.config['EXCEL_FILE_PATH'])
-
     return app
 
 def table_exists(table_name):
     inspector = db.inspect(db.engine)
     return inspector.has_table(table_name)
 
+
+def is_table_empty():
+    return db.session.query(Word).count() == 0
 
 
 def import_data_from_excel(file_path):
